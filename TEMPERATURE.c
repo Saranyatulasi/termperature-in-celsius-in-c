a82e1e9b283d77c6aa9cc60c6bfc4e83ff565f23
Naga Saranya tulasi haridasu <saranyatulasiharidasu@gmail.com>
2025-03-21 22:49:39 +0530

    Add files via upload

---
+++ TEMPERATURE.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+
+	float F,C;
+	printf("enter the value of celcius");
+	scanf ("%f",&C);
+    F=((C*9/5)+32);
+	printf("celcius to farhanheat is %.1f",F);
+	
+}
